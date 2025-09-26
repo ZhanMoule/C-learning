fdc9a0323f566299601823af7c9c80c9c0a21070
ZhanMoule <a161706036@163.com>
2025-09-26 14:09:04 +0800

    链表

---
+++ Cpp题库/链表.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+struct node{
+    int value;
+    struct node* next;
+};
+node* list_creat(int arr[],int n){
+    node* list;
+    return list;
+}
+int main(){
+    int data[]={1,2,3,4,5,6,7,8,9,10};
+    list_creat(data,10);
+    return 0;
+}
